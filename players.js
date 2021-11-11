77a824e72559e46a9b68f2de25aec658e147c0f7
dahvsface <62912096+dahvsface@users.noreply.github.com>
2021-11-11 08:44:40 -0800

    Add files via upload

---
+++ players.js
@@ -0,0 +1,32 @@
+module.exports = function() {
+
+    var express = require('express');
+    var router = express.Router();
+
+    function getPlayers(res, mysql, context, complete) {
+        mysql.pool.query("SELECT * FROM Players", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.send();
+            }
+            context.players = results;
+            complete();
+        })
+    }
+
+    router.get('/', function (req, res, next) {
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getPlayers(res, mysql, context, complete);
+
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 2) {
+                res.render('players', context);
+            }
+        }
+    });
+}
+
+
