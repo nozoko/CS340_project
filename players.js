6ffa3163d031b8bf4e7d03a9894fbbc6dc01a90f
dahvsface <62912096+dahvsface@users.noreply.github.com>
2021-11-14 21:03:38 -0800

    Add files via upload

---
+++ players.js
@@ -0,0 +1,62 @@
+module.exports = function() {
+
+    var express = require('express');
+    var router = express.Router();
+
+    function getPublishers(res, mysql, context, complete){
+        mysql.pool.query("SELECT publisherID as publisherID, publisherName FROM Publishers", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.employer  = results;
+            complete();
+        });
+    }
+
+    function getPlayers(res, mysql, context, complete) {
+        mysql.pool.query("SELECT Players.playerID as playerID, email, firstName, lastName, gamerTag, Publishers.publisherName AS employer FROM Players " +
+            "INNER JOIN Publishers ON employer = Publishers.publisherID", function (error, results, fields) {
+            if (error) {
+                res.write(JSON.stringify(error));
+                res.send();
+            }
+            context.players = results;
+            complete();
+        })
+    }
+
+    router.get('/', function (req, res, next) {
+        var callbackCount = 0;
+        var context = {};
+        var mysql = req.app.get('mysql');
+        getPlayers(res, mysql, context, complete);
+        getPublishers(res, mysql, context, complete);
+        function complete() {
+            callbackCount++;
+            if (callbackCount >= 2) {
+                res.render('players', context);
+            }
+        }
+    });
+
+    router.post('/', function(req, res){
+        console.log(req.body)
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO Players (email, firstName, lastName, gamerTag, employer) VALUES (?,?,?,?,?)";
+        var inserts = [req.body.email, req.body.firstName, req.body.lastName, req.body.gamerTag];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/players');
+            }
+        });
+    });
+
+    return router;
+}();
+
+
