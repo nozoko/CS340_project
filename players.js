7e5723208bf23c8fb3117b03852e23de4826835f
dahvsface <62912096+dahvsface@users.noreply.github.com>
2021-11-23 11:44:42 -0800

    Update players.js

--- players.js
+++ players.js
@@ -88,7 +88,24 @@ module.exports = function() {
         }
     });
 
+    router.delete('/:playerID', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM Players WHERE playerID = ?";
+        var inserts = [req.params.playerID];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })    
+    
     return router;
+    
 }();
 
 
