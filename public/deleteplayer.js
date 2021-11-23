fa5798075a32ddb90031ebf32a4765bfe68c51bd
dahvsface <62912096+dahvsface@users.noreply.github.com>
2021-11-23 11:46:57 -0800

    Add files via upload

---
+++ public/deleteplayer.js
@@ -0,0 +1,25 @@
+
+function deletePlayer(playerID){
+    $.ajax({
+        url: '/players/' + playerID,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
+
+function deletePlayersGames(playerID, gameID){
+    $.ajax({
+        url: '/playersGames/playerID/' + playerID + '/gameID/' + gameID,
+        type: 'DELETE',
+        success: function(result){
+            if(result.responseText != undefined){
+                alert(result.responseText)
+            }
+            else {
+                window.location.reload(true)
+            }
+        }
+    })
+};
